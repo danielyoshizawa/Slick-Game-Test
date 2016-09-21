acdc4625bc423cb4a82ba1f7389e5efc34d13bf7
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-09-21 13:39:02 -0300

    Implementing KeyListener

--- src/game/World.java
+++ src/game/World.java
@@ -1,25 +1,40 @@
 package game;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
 
-public class World extends BasicGame {
+public class World extends BasicGame implements KeyListener {
+
+    Circle circle;
+    float x = 0.0f;
+    float y = 0.0f;
 
     public World(String title) {
         super(title);
     }
 
     public void init(GameContainer gameContainer) throws SlickException {
-
+        circle = new Circle(100, 150, 50);
     }
 
     public void update(GameContainer gameContainer, int i) throws SlickException {
-
+        circle.setLocation(x,y);
     }
 
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
 
+        graphics.drawString("Hello Jacqueline", 10, 100);
+        graphics.draw(circle);
+    }
+
+    public void keyPressed(int key, char c) {
+        if (c == 'a')
+            x -= 10;
+        if (c == 'd')
+            x += 10;
+        if (c == 'w')
+            y -= 10;
+        if (c == 's')
+            y += 10;
     }
 }
